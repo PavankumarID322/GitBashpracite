3d4eab4c8efc8b520017a81afbec321686cabea9
PavankumarID322 <pavankumarkulkarni33@gmail.com>
2023-06-01 12:51:06 +0530

    update Demo class

---
+++ src/test/java/com/actitime/testscript/GitBashPratice.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.annotations.Test;
+
+public class GitBashPratice {
+
+	@Test
+	public void demo() {
+		System.out.println("vkjdbvksdj");
+	}
+}
